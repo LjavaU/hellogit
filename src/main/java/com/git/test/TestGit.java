d9a26fb7a2b3b650fbb5d8abf9768d1396c60f0a
luhao <1666831357@qq.com>
2020-02-12 17:24:31 +0800

    小白

--- src/main/java/com/git/test/TestGit.java
+++ src/main/java/com/git/test/TestGit.java
@@ -3,6 +3,6 @@ package com.git.test;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("这是第一次测试");
-        System.out.println("这是第二次测试");
+
     }
 }
